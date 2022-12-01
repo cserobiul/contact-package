b38451190f5e0b772c8f3bfeb7a310554d8448aa
cserobiul <abishar14@gmail.com>
2022-12-02 04:21:59 +0600

    form design modify - bootstrap design

--- src/views/contact.blade.php
+++ src/views/contact.blade.php
@@ -6,15 +6,33 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contact</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Contact us any time</h1>
-    <form action="{{ route('contact') }}" method="post">
-        @csrf
-        Name: <input type="text" name="name" placeholder="e.g: robiul islam"> <br> <br>
-        Email: <input type="email" name="email" placeholder="e.g: robiulislam@gmail.com"> <br> <br>
-        Message: <textarea name="message" id="" cols="30" rows="10" placeholder="Type your message"></textarea> <br> <br>
-        <button type="submit">Send Mail</button>
-    </form>
+
+
+    <div class="container">
+        <div class="row">
+            <h2>Contact us any time</h2>
+        </div>
+        <div class="row">
+            <form action="{{ route('contact') }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="name" class="form-control" id="name" placeholder="e.g: robiul islam">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" placeholder="dev@apol.com.bd">
+                </div>
+                <div class="form-group">
+                    <label for="message">Messages</label>
+                    <textarea class="form-control" id="message" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Send Email</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
